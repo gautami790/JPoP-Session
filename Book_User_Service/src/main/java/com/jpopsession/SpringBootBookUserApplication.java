1738e289466805ee5b4576f273b9e11cab8b8b32
gshitole <gshitole@pulsepoint.com>
2021-12-15 18:17:54 +0530

    Week1: Added @SpringBootApplication and @Entity Annotations

--- Book_User_Service/src/main/java/com/jpopsession/SpringBootBookUserApplication.java
+++ Book_User_Service/src/main/java/com/jpopsession/SpringBootBookUserApplication.java
@@ -1,10 +1,10 @@
+package com.jpopsession;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@EnableAutoConfiguration
+@SpringBootApplication
 public class SpringBootBookUserApplication {
 
     public static void main(String[] args) {
