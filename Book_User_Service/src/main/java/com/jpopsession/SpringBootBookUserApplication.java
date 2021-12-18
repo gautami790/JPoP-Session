4cdea707b144cc07500145015a27cab0d228a09a
gshitole <gshitole@pulsepoint.com>
2021-12-18 20:34:57 +0530

    Week2: Service Discovery

--- Book_User_Service/src/main/java/com/jpopsession/SpringBootBookUserApplication.java
+++ Book_User_Service/src/main/java/com/jpopsession/SpringBootBookUserApplication.java
@@ -3,8 +3,10 @@ package com.jpopsession;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringBootBookUserApplication {
 
     public static void main(String[] args) {
