788d15f8714c25a7d008401cf3e51ab85c703647
gshitole <gshitole@pulsepoint.com>
2022-01-05 12:08:23 +0530

    Change the server configuration

--- Book_User_Service/src/main/java/com/jpopsession/controller/KafkaController.java
+++ Book_User_Service/src/main/java/com/jpopsession/controller/KafkaController.java
@@ -14,5 +14,6 @@ public class KafkaController {
     @PostMapping("/postMessage")
     public void sendMessage(@RequestBody String msg){
         producer.publishToTopic(msg);
+        System.out.println(msg);
     }
 }
